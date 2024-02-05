1c072ba01311bff1ace9b3dd0a943bf308ff53b6
madunS <muhammadon20m@gmail.com>
2024-02-05 22:53:42 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,8 @@
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+
+
 // cara 1
 // function grow (x) {
 //     let result = x[0];
