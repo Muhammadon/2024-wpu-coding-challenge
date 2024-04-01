efd39aa58b19ca05fc7740b7caf319b570f17609
madunS <muhammadon20m@gmail.com>
2024-04-01 22:29:02 +0700

    changes # Day 10 / 366

--- 10-stringToArray.js
+++ 10-stringToArray.js
@@ -1,3 +1,8 @@
+// WPU Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript
+
+
 const stringToArray = (string) => {
     return string.split(' ');
 }
